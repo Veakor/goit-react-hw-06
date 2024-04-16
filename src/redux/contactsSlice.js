c43add7316762e7abddca5351a8b1875ff5d15ef
Veakor <135655247+Veakor@users.noreply.github.com>
2024-04-16 17:38:17 +0200

    add

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
-    name: ''
+    items: []
   },
   reducers: {
     addContact: (state, action) => {
--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -14,6 +14,6 @@ const filtersSlice = createSlice({
 
 export const { changeFilter } = filtersSlice.actions;
 
-export const selectNameFilter = state => state.contacts.name;
+export const selectNameFilter = state => state.filters.name;
 
 export default filtersSlice.reducer;
